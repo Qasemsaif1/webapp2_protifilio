096f7e468166762fb8bb9904a0fdeada891b291e
Qasem <qasemsaiff@gmail.com>
2023-04-01 21:15:56 +0200

    created the columns, titles and images

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import pandas
 import streamlit as st
-import os
+import pandas as pd   # This library helps reading the data in the csv
 
 st.set_page_config(layout='wide')# to change the layout of the page
 
@@ -14,5 +15,23 @@ with col2:
     st.title("Qasem Saif")
     content = " I am a sustainability consultant working in Europe"
     st.info(content)
+
+
+col3,  col4 = st.columns(2)
+
+data = pandas.read_csv('data.csv', sep=";") # storing the data in this variabale. Sep is the columns divider
+
+
+with col3:
+    for index, row in data[:10].iterrows():     # iterrows() function eterates on every raw of the file
+        titles = st.header(row['title'])
+        images = st.image(f"images/{row['image'][:10]}")
+
+
+
+with col4:
+    for index, row in data[10:].iterrows():  # iterrows() function eterates on every raw of the file
+        titles = st.header(row['title'])
+        images = st.image(f"images/{row['image']}")
 
 
